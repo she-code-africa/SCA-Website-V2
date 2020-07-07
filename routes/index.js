3b49fa1d2e7249525fdfe2b861e8013ca4e61697
temilolakutelu <temilolakutelu@gmail.com>
2020-07-07 21:09:18 +0100

    code environment setup

--- routes/index.js
+++ routes/index.js
@@ -28,22 +28,22 @@ keystone.pre('render', middleware.flashMessages);
 
 // Import Route Controllers
 var routes = {
-	views: importRoutes('./views'),
+    views: importRoutes('./views'),
 };
 
 // Setup Route Bindings
-exports = module.exports = function (app) {
-	// Views
-	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
-	app.get('/about', routes.views.about);
-	app.get('/jobs', routes.views.jobs);
-	app.get('/community', routes.views.community);
-	app.get('/gallery', routes.views.gallery);
-	app.all('/contact', routes.views.contact);
+exports = module.exports = function(app) {
+    // Views
+    app.get('/', routes.views.index);
+    app.get('/blog/:category?', routes.views.blog);
+    app.get('/blog/post/:post', routes.views.post);
+    app.get('/about', routes.views.about);
+    app.get('/jobs', routes.views.jobs);
+    app.get('/community', routes.views.community);
+    app.get('/gallery', routes.views.gallery);
+    app.all('/contact', routes.views.contact);
 
-	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
-	// app.get('/protected', middleware.requireUser, routes.views.protected);
+    // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+    // app.get('/protected', middleware.requireUser, routes.views.protected);
 
 };
