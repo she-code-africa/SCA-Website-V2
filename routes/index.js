48248768e2b6eb4402e26505dac1e920b6b24345
temilolakutelu <temilolakutelu@gmail.com>
2020-07-27 20:58:05 +0100

    teampage ui design implementation

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,7 @@ var routes = {
 exports = module.exports = function(app) {
     // Views
     app.get('/', routes.views.index);
+    app.get('/team', routes.views.team);
     // app.get('/blog/:category?', routes.views.blog);
     // app.get('/blog/post/:post', routes.views.post);
     // app.get('/about', routes.views.about);
