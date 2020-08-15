9b04cd0fefdb20b909aa8fe480ec32afda58a7c7
Mary Mazi <mazi.mary.o@gmail.com>
2020-08-15 09:08:26 -0500

    implement donate/partner ui page

---
+++ routes/views/donatepartner.js
@@ -0,0 +1,13 @@
+var keystone = require('keystone');
+
+exports = module.exports = function (req, res) {
+
+    var view = new keystone.View(req, res);
+    var locals = res.locals;
+
+    // item in the header navigation.
+		locals.section = 'partners';
+		
+    // Render the view
+    view.render('donatepartner');
+};
