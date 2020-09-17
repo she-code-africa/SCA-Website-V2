3d859a265219ad58e18459beb9987c30a2471528
maryam-olabisi <m.olabisimurit@gmail.com>
2020-09-17 12:22:44 +0100

    corrected made

--- routes/views/about.js
+++ routes/views/about.js
@@ -1,10 +1,24 @@
 var keystone = require('keystone');
+var Team = keystone.list('Team');
 
 exports = module.exports = function (req, res) {
 
     var view = new keystone.View(req, res);
     var locals = res.locals;
-
+    locals.section = 'about';
+    locals.data = {
+        teams: [],
+        // membership: [],
+    };
+    view.on('init', function(next) {
+        var q = 
+        keystone.list('TeamCategory').model.find().exec(function(err, result) {
+            locals.data.membership = result;
+            next(err);
+        });
+    });
+	view.query('teams', Team.model.find().where('state', 'published').sort('publishedDate').limit(6));
+    console.log(locals.data.teams)
     // Render the view
     view.render('about');
 };
