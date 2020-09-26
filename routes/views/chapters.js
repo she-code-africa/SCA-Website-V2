c4f41ef615ca2f25cd6a9a9fb26ecd5ffaa4179c
odunayo <okebunmiodunayo@gmail.com>
2020-09-26 12:35:46 +0100

    implement search functionality

--- routes/views/chapters.js
+++ routes/views/chapters.js
@@ -1,26 +1,40 @@
 var keystone = require('keystone');
 var Chapter = keystone.list('Chapter');
-var ChapterCategory = keystone.list('ChapterCategory')
-exports = module.exports = function(req, res) {
+var ChapterCategory = keystone.list('ChapterCategory');
+exports = module.exports = function (req, res) {
 
-    var view = new keystone.View(req, res);
-    var locals = res.locals;
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
 
-    // item in the header navigation.
-    locals.section = 'chapters';
-    locals.data = {
-        chapters: [],
-        category: [],
-    };
-    // view.on('init', function(next) {
-    //     keystone.list('ChapterCategory').model.find().exec(function(err, result) {
-    //         locals.data.category = result;
-    //         next(err);
-    //     });
-    // });
-    view.query('category', ChapterCategory.model.find());
-    view.query('chapters', Chapter.model.find().where('state', 'published').sort('publishedDate'));
+	// item in the header navigation.
+	locals.section = 'chapters';
+	locals.formData = req.body || {};
+	locals.validationErrors = {};
+	locals.chapterSubmitted = false;
+	locals.error = null;
+	locals.data = {
+		chapters: [],
+		category: [],
+	};
+	var searchQuery = req.body.searchCountry;
+	view.on('post', { action: '' }, function (next) {
+		Chapter.model.find().where('chapterName', `${searchQuery}`).sort('publishedDate').exec(function (err, result) {
+			locals.data.chapters = result;
+			// next(err);
+		});
+		next();
+		req.body.searchCountry = '';
+	});
 
-    // Render the view
-    view.render('chapters');
+	// view.on('init', function (next) {
+	// 	keystone.list('ChapterCategory').model.find().exec(function (err, result) {
+	// 		locals.data.category = result;
+	// 		console.log(result);
+	// 		next(err);
+	// 	});
+	// });
+	view.query('category', ChapterCategory.model.find());
+	view.query('chapters', Chapter.model.find().where('state', 'published').sort('publishedDate'));
+	// Render the view
+	view.render('chapters');
 };
