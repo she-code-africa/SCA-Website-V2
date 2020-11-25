684ed574039367c0d71c40d5fb259522ab833962
temilolakutelu <temilolakutelu@gmail.com>
2020-11-25 14:57:34 +0100

    set jobs view by published state

--- models/Job.js
+++ models/Job.js
@@ -7,21 +7,22 @@ var Types = keystone.Field.Types;
  */
 
 var Job = new keystone.List('Job', {
-	map: { name: 'title' },
-	autokey: { path: 'slug', from: 'title', unique: true },
+    map: { name: 'title' },
+    autokey: { path: 'slug', from: 'title', unique: true },
 });
 
 Job.add({
-	title: { type: String, initial: true, required: true },
-	categories: { type: Types.Relationship, ref: 'JobCategory', many: false },
-	deadline: { type: Types.Date, index: true },
-	publishedDate: { type: Types.Date, index: true, default: Date.now },
-	minimumExperience: { type: String, initial: true, required: true },
-	jobDescription: { type: Types.Textarea },
-	applicationLink: { type: String, initial: true, required: true },
-	// applicationEmail: { type: Types.Email, initial: true, required: true },
-	salaryRange: { type: String },
-	company: { type: Types.Relationship, ref: 'Company', index: true },
+    title: { type: String, initial: true, required: true },
+    categories: { type: Types.Relationship, ref: 'JobCategory', many: false },
+    deadline: { type: Types.Date, index: true },
+    state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+    publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+    minimumExperience: { type: String, initial: true, required: true },
+    jobDescription: { type: Types.Textarea },
+    applicationLink: { type: String, initial: true, required: true },
+    // applicationEmail: { type: Types.Email, initial: true, required: true },
+    salaryRange: { type: String },
+    company: { type: Types.Relationship, ref: 'Company', index: true },
 });
 
 Job.defaultColumns = 'title, state|20%, deadline|20%, publishedDate|20%';
