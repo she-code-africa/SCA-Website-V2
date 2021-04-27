d80aaa75ceb1a3ab1f4645ea3668e5bbedc29c3f
Temilola Kutelu <temilolakutelu@gmail.com>
2021-04-27 08:59:38 +0100

    Revert "Revert "Feature/job opportunity""

--- routes/views/jobsorgdashboard.js
+++ routes/views/jobsorgdashboard.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
+
 const cons = require('consolidate');
 var keystone = require('keystone');
 const { keys } = require('lodash');
 var localStorage = require('../../utils/localStorage');
 var Job = keystone.list('Job');
+var paginate = require('../../utils/paginate');
 
-exports = module.exports = function(req, res) {
-    const companyData = localStorage.getItem('loggedInCompany') || "";
+exports = module.exports = function (req, res) {
+    const { cookieTag } = req.decoded || {};
+
+    const companyData = localStorage.getItem(`loggedInCompany-${cookieTag}`) 
+        ? localStorage.getItem(`loggedInCompany-${cookieTag}`) 
+        : "";
     if (companyData === "") {
         return res.redirect('/jobs');
     } else if (companyData !== req.params.org) {
@@ -18,15 +25,28 @@ exports = module.exports = function(req, res) {
     // item in the header navigation.
     locals.section = 'jobs';
     locals.data = {
-        company: [],
-        jobs: [],
+        company: {},
+        allPublishedJobs: {},
+        unpublishedJobs: {},
+        companyName: localStorage.getItem(`loggedInCompany-${cookieTag}`) || "",
     };
+    locals.dateDiff = function (deadline) {
+        jobsDeadline = new Date(deadline);
+        today = new Date();
+        return Math.floor(
+            (Date.UTC(jobsDeadline.getFullYear(), jobsDeadline.getMonth(), jobsDeadline.getDate()) 
+            - Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) ) 
+            / (1000 * 60 * 60 * 24)
+        );
+    };
+    locals.paginationMetadataPublished = {};
+    locals.paginationMetadataUnpublished = {};
 
     //company details
-    view.on('init', function(next) {
+    view.on('init', function (next) {
         var q = keystone.list('Company').model.findOne({ slug: req.params.org });
 
-        q.exec(function(err, result) {
+        q.exec(function (err, result) {
             if (result != null) {
                 locals.data.company = result;
             } else {
@@ -37,25 +57,65 @@ exports = module.exports = function(req, res) {
 
     });
 
-    // all jobs events
-    view.on('init', function(next) {
-        Job.paginate({
-                page: req.query.page || 1,
-                perPage: 5,
-                filters: {
-                    company: locals.data.company,
-                },
+    // all published jobs events
+    view.on('init', async function (next) {
+        let currentPage = req.query.section == 'published' || req.query.section == 'all'
+            ? Number(req.query.page) || 1
+            : 1;
+        let pagesize = Number(process.env.JOBS_PAGE_SIZE);
+        let firstPage = 1;
+        let totalJobsCount = await Job.model.count({state: 'published', company: locals.data.company}, function (err, count) {
+            return count;
+        });
 
-            })
+        Job.model.find({state: 'published', company: locals.data.company})
+            .skip(pagesize * (currentPage - 1))
+            .limit(pagesize)
             .sort('publishedDate')
-            .populate('company categories')
-            .exec(function(err, results) {
-                locals.data.jobs = results;
+            .populate({
+                path: 'company categories', populate: ['comapny.categories', 'categories'],
+            })
+            .exec(async function (err, results) {
+                locals.data.allPublishedJobs = results;
+                locals.data.paginationMetadataPublished = await paginate(
+                    currentPage,
+                    firstPage,
+                    pagesize,
+                    totalJobsCount
+                );
                 next(err);
             })
-
     });
 
+    // all unpublished jobs events
+    view.on('init', async function (next) {
+        let currentPage = req.query.section == 'unpublished' || req.query.section == 'all'
+            ? Number(req.query.page) || 1
+            : 1;
+        let pagesize = Number(process.env.JOBS_PAGE_SIZE);
+        let firstPage = 1;
+        let totalJobsCount = await Job.model.count({state: ['draft', 'archived'], company: locals.data.company}, function (err, count) {
+            return count;
+        });
+
+        Job.model.find({state: ['draft', 'archived'], company: locals.data.company})
+            .skip(pagesize * (currentPage - 1))
+            .limit(pagesize)
+            .sort('publishedDate')
+            .populate({
+                path: 'company categories', populate: ['comapny.categories', 'categories'],
+            })
+            .exec(async function (err, results) {
+                locals.data.unpublishedJobs = results;
+                locals.data.paginationMetadataUnpublished = await paginate(
+                    currentPage,
+                    firstPage,
+                    pagesize,
+                    totalJobsCount,
+                );
+                next(err);
+            })
+    });
     // Render the view
     view.render('jobsorgdashboard');
 };
--- routes/views/jobsregister.js
+++ routes/views/jobsregister.js
@@ -2,10 +2,9 @@ var keystone = require('keystone');
 var localStorage = require('../../utils/localStorage');
 // var Company = keystone.list('Company');
 var CompanyCategory = keystone.list('CompanyCategory');
+const generateRandomString = require('../../utils/generateRandomString');
 
-exports = module.exports = function(req, res) {
-    localStorage.removeItem('loggedInCompany');
-
+exports = module.exports = function (req, res) {
     var view = new keystone.View(req, res);
     var locals = res.locals;
     locals.data = {
@@ -14,30 +13,40 @@ exports = module.exports = function(req, res) {
     // item in the header navigation.
     locals.section = 'jobs';
     locals.formData = req.body || {};
-    // locals.validationErrors = {};
+    // locals.validationErrors = {}; 
 
-    view.on('init', function(next) {
-        CompanyCategory.model.find().sort('name').exec(function(err, result) {
-            locals.data.categories = result;
-            next(err);
-        });
-    });
+    // view.on('init', function (next) {
+    //     CompanyCategory.model.find().sort('name').exec(function (err, result) {
+    //         locals.data.categories = result;
+    //         next(err);
+    //     });
+    // });
 
     // on adding company
-    view.on('post', { action: '' }, function() {
+    view.on('post', { action: '' }, function () {
+        // console.log(locals.formData);
         const dataToSave = {
             name: locals.formData.org_name,
-            industry: locals.formData.org_industry,
+            // industry: locals.formData.org_industry,
             website: locals.formData.website_url,
             address: locals.formData.org_address,
             location: locals.formData.location,
         };
+        // if (locals.formData.org_industry === 'other') {
+        //     dataToSave.categories = ''
+        //     dataToSave.industry = locals.formData.industry
+        // } else {
+        //     dataToSave.categories = locals.formData.org_industry
+        // }
+        // console.log(dataToSave);
         const dataToSaveString = JSON.stringify(dataToSave);
 
         // save to session
-        localStorage.setItem('companyData', dataToSaveString);
-        return res.redirect('/jobs/register/contact-details');
+        const pageId = generateRandomString();
+        let hash = Buffer.from(pageId).toString('base64');
 
+        localStorage.setItem(`companyData-${hash}`, dataToSaveString);
+        return res.redirect(`/jobs/register/contact-details?hash=${hash}`);
     });
 
     // Render the view
