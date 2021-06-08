714f982f81dfa2e11e26b3809442bfa8a8ef30ca
Mary Mazi <mazi.mary.o@gmail.com>
2021-06-08 05:36:51 -0500

    remove job detail modals and add details page

---
+++ routes/views/jobdetail.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+var keystone = require('keystone');
+var Job = keystone.list('Job');
+
+exports = module.exports = function (req, res) {
+    var view = new keystone.View(req, res);
+    
+    var locals = res.locals;
+
+    locals.section = 'jobs';
+    locals.data = {
+        job: {},
+        todayDate: new Date(),
+    };
+    locals.dateDiff = function (deadline) {
+        jobsDeadline = new Date(deadline);
+        today = new Date();
+        return Math.floor(
+            (Date.UTC(jobsDeadline.getFullYear(), jobsDeadline.getMonth(), jobsDeadline.getDate()) 
+            - Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) ) 
+            / (1000 * 60 * 60 * 24)
+        );
+    };
+
+    //job details
+    view.on('init', function (next) {
+        Job.model.findOne({ _id: req.params.id })
+            .populate({
+                path: 'company categories', populate: ['comapny.categories', 'categories'],
+            })
+            .exec(async function (err, result) {
+                if (result != null) {
+                    locals.data.job = result;
+                } else {
+                    return res.status(404);
+                }
+                next(err);
+            })
+
+    });
+
+    // Render the view
+    view.render('jobdetail');
+};
