da89bdef2169034f0cb406c928fb9bf15a4a65fa
Mary Mazi <mazi.mary.o@gmail.com>
2021-07-10 03:22:05 -0500

    email notification on job submission and approval

--- models/Job.js
+++ models/Job.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
+var Company = keystone.list('Company');
 // var deepPopulate = require('mongoose-deep-populate')(keystone.mongoose);
 // Job.schema.plugin(deepPopulate);
 
@@ -27,6 +28,97 @@ Job.add({
     salaryRange: { type: String },
     company: { type: Types.Relationship, ref: 'Company', index: true },
 });
+
+Job.schema.methods.sendNotificationEmail = async function (emailPayload, callback) {
+    let jobOwner;
+    const job = this;
+    const brand = keystone.get('brand');
+    if (typeof callback !== 'function') {
+        callback = function(err) {
+            if (err) {
+                console.error('There was an error sending the notification email:', err);
+            }
+        };
+    }
+
+    if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
+        console.log('Unable to send email - no mailgun credentials provided');
+        return callback(new Error('could not find mailgun credentials'));
+    }
+
+    await Company.model.findById(job.company).exec(function (err, result) {
+        if (result != null) {
+            jobOwner = {
+                email: result.email,
+                name: { first: result.companyName },
+            };
+        }
+    });
+
+    const communityManagers = await keystone.list('Team').model.find({ state: 'published', role: 'Community Manager' });
+
+    keystone.list('User').model.find({
+        $and: [
+            { 'name.first': { $in: communityManagers.map(function (manager) { return manager.name.split(' ')[0]; }) }},
+            { 'name.last': { $in: communityManagers.map(function (manager) { return manager.name.split(' ')[1]; }) }},
+        ]
+    }).exec(function(err, admins) {
+        if (err) return callback(err);
+        if (job.isNew && job.state === 'draft') {
+            new keystone.Email({
+                templateName: 'job-posted-admin-notification',
+                transport: 'mailgun',
+            }).send({
+                to: admins,
+                from: {
+                    name: brand,
+                    email: 'info@shecodeafrica.org',
+                },
+                subject: 'A new job is awaiting your approval',
+                job,
+            }, callback);
+
+            new keystone.Email({
+                templateName: 'job-posted-notification',
+                transport: 'mailgun',
+            }).send({
+                to: jobOwner,
+                from: {
+                    name: brand,
+                    email: 'info@shecodeafrica.org',
+                },
+                subject: 'Your new job posting has been received',
+                job,
+            }, callback);
+        }
+        if (job.isModified('state') && job.state === 'published') {
+            new keystone.Email({
+                templateName: 'job-published-notification',
+                transport: 'mailgun',
+            }).send({
+                to: jobOwner,
+                from: {
+                    name: brand,
+                    email: 'info@shecodeafrica.org',
+                },
+                subject: 'Your New Job Posting is Now Active',
+                job,
+            }, callback);
+        }
+    });
+};
+
+Job.schema.pre('save', function (next) {
+    let job = this;
+    if (job.isNew && job.state === 'draft') {
+        this.sendNotificationEmail();
+    }
+    if (job.isModified('state') && job.state === 'published') {
+        this.sendNotificationEmail();
+    }
+    return next();
+});
+
 
 Job.defaultColumns = 'title, state|20%, deadline|20%, publishedDate|20%';
 Job.register();
