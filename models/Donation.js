b8160be3d043fe2a924d607c8706fbc26d514f24
temilolakutelu <temilolakutelu@gmail.com>
2023-03-16 16:27:46 +0100

    website corrections

---
+++ models/Donation.js
@@ -0,0 +1,14 @@
+var keystone = require('keystone');
+
+
+var Donation = new keystone.List('Donation');
+
+Donation.add({
+    currency: { type: String, initial: true, required: true },
+    amount: { type: String, initial: true, required: true },
+
+});
+
+Donation.track = true;
+Donation.defaultColumns = 'currency, amount';
+Donation.register();
